c528261f2e7b25ff2a74f71d93882ea36a57e273
Minhnebanme <108916329+Minhnebanme@users.noreply.github.com>
2024-03-19 04:50:24 +0700

    Update MashBalloons.lua

--- MashBalloons.lua
+++ MashBalloons.lua
@@ -1,3 +1,15 @@
+getgenv().autoBalloon = true
+
+getgenv().autoBalloonConfig = {
+
+    START_DELAY = 1, -- delay before starting
+    SERVER_HOP = true, -- server hop after popping balloons
+    SERVER_HOP_DELAY = 0, -- delay before server hopping
+    BALLOON_DELAY = 2, -- delay before popping next balloon (if there are multiple balloons in the server)
+    GET_BALLOON_DELAY = 1, -- delay before getting balloons again if none are detected
+    WAIT_FOR_BREAK = 4 -- delay in seconds to wait for the gift to break
+}
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/waitForGameLoad.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/cpuReducer.lua"))()
 workspace.Map.DescendantAdded:Connect(function()
